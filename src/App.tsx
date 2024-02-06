9e30fe3d11601f1561e4f051c373550d4f67edcf
Miguel Rodrigues <miguel.arodrigues22@gmail.com>
2024-02-06 10:46:25 +0000

    added pages && login routes && redux && tailwind && localstorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import LoginPage from './components/LoginPage';
+import GamePage from './components/GamePage';
+import ScorePage from './components/ScorePage';
+import PrivateRoutes from './utils/PrivateRoutes';
+import { useSelector } from 'react-redux';
 
-function App() {
+const App = () => {
+  const [userName, setUserName] = useState('');
+  const [authenticated, setAuthenticated] = useState(false);
+
+  const { users } = useSelector((state: any) => state.users);
+  const loggedInUser = useSelector((state: any) => state.loggedInUser.loggedInUser);
+
+  console.log('loggedInUser APP', loggedInUser)
+  console.log('entrou',)
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Routes>
+          <Route path="/" element={loggedInUser ? (<Navigate to="/game" />)
+            :
+            (<LoginPage />)
+          }
+          />
+          <Route
+            path="/"
+            element={loggedInUser ? <PrivateRoutes /> : <Navigate to="/" />}
+          >
+            <Route path='game' element={<GamePage />} />
+            <Route path="game/score" element={<ScorePage />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
+
