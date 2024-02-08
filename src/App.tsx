6968f1438b8b3a3a090e923ec81287cf3500cb02
Miguel Rodrigues <miguel.arodrigues22@gmail.com>
2024-02-08 12:29:21 +0000

    game almost finished && added images from pexels && high scores page WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
-import GamePage from './components/GamePage';
 import ScorePage from './components/ScorePage';
 import PrivateRoutes from './utils/PrivateRoutes';
 import { useSelector } from 'react-redux';
+import GamePage from './components/GamePage';
+import axios from 'axios';
 
 const App = () => {
   const [userName, setUserName] = useState('');
@@ -12,6 +13,50 @@ const App = () => {
 
   const { users } = useSelector((state: any) => state.users);
   const loggedInUser = useSelector((state: any) => state.loggedInUser.loggedInUser);
+
+  const [pexelCards, setPexelCards] = useState([])
+
+  const [defaultImage, setDefaultImage] = useState(null)
+
+
+  const fetchSingleImage = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.pexels.com/v1/curated?per_page=1`,
+        { headers: { Authorization: '84CptlFQLYfWj0euXgTzdGohgvSjyaG03MDtDgTLnK6CITNCybJKGjTZ' } }
+      );
+      setDefaultImage(response.data.photos)
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }
+  };
+
+  const fetchImages = async () => {
+    try {
+      const response = await axios.get(
+        "https://api.pexels.com/v1/curated?per_page=6",
+        { headers: { Authorization: '84CptlFQLYfWj0euXgTzdGohgvSjyaG03MDtDgTLnK6CITNCybJKGjTZ' } }
+      );
+      const images = response.data.photos.slice(0, 6).map(photo => photo.src.medium);
+      const duplicatedImages = [...images, ...images];
+      const shuffledImages = duplicatedImages.sort(() => Math.random() - 0.5);
+      setPexelCards(
+        shuffledImages.map((url) => ({ url, matched: false }))
+          .map((card) => ({ ...card, id: Math.random() }))
+      );
+
+      // console.log('pexelCards', pexelCards)
+    } catch (error) {
+      console.error('Error fetching images:', error);
+    }
+  };
+
+  useEffect(() => {
+
+    fetchSingleImage()
+    fetchImages();
+
+  }, []);
 
   return (
     <div className="App">
@@ -26,7 +71,7 @@ const App = () => {
             path="/"
             element={loggedInUser ? <PrivateRoutes /> : <Navigate to="/" />}
           >
-            <Route path='game' element={<GamePage />} />
+            <Route path='game' element={<GamePage defaultImage={defaultImage} pexelCards={pexelCards} />} />
             <Route path="game/score" element={<ScorePage userScore={null} onClose={null} />} />
           </Route>
         </Routes>
--- src/components/ScorePage.tsx
+++ src/components/ScorePage.tsx
@@ -4,13 +4,16 @@ import { useSelector } from 'react-redux';
 export default function ScorePage({ onClose, userScore }) {
 
     const allUsers = useSelector((state: any) => state.users.users);
+    console.log('allUsers', allUsers)
+
+    const sortedUsers = allUsers.slice().sort((a, b) => a.score - b.score);
 
     return (
-        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50">
+        <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50 z-50">
             <div className="bg-white p-8 rounded-md">
-                <h1 className="text-2xl font-bold mb-4">Scores</h1>
-                {allUsers.map((user) => (
-                    <h1>{user.username} - {user.score}</h1>
+                <h1 className="text-2xl font-bold mb-4">High Scores</h1>
+                {sortedUsers.map((user, index) => (
+                    <h1 key={index}>{user.username} - {user.score}</h1>
                 ))}
 
                 <button onClick={onClose} className="p-2 bg-gray-200 rounded-md hover:bg-gray-300 transition duration-300">
