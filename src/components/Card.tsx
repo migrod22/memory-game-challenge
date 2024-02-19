a30dbe5d47a951ec82dc3cff6fdb526c7e762a6c
Miguel Rodrigues <miguel.arodrigues22@gmail.com>
2024-02-19 15:16:39 +0000

    .

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,7 +9,7 @@ export default function Card({ card, handleChoice, turned, disabled }) {
     return (
         <div onClick={handleClick}>
             <div className={`transform transition-transform ${turned ? "rotate-y-0" : "rotate-y-90"}`}>
-                <img className="block border-2 border-white rounded h-60 w-60" src={turned ? card.url : "/img/memoryGame.jpeg"} />
+                <img alt="card" className="block border-2 border-white rounded h-60 w-60" src={turned ? card.url : "/img/memoryGame.jpeg"} />
             </div>
         </div>
     )
